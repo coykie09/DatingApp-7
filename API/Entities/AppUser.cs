d080f775e514c44b36b17fc14414e27d2f6a0712
rssegarino <rssegarino@gmail.com>
2023-01-22 05:54:02 +0800

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        
+    }
+}
